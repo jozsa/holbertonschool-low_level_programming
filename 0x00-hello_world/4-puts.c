c9185b99c46d626a9c42c8e056f0d43d172bac10
jozsa <allisonjoyweiner@gmail.com>
2018-09-12 20:54:25 +0000

    Revised C soure code to match Betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Prints ""Programming is like building a multilingual puzzle"
+ * 
+ * Return: Always 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
