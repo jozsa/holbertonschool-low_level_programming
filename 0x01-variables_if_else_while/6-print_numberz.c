4c05d326f90beabf56c87b4ab4f12300227e8af2
jozsa <allisonjoyweiner@gmail.com>
2018-09-14 02:33:26 +0000

    Printing numbers in base 10 using a while loop

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+
+/**
+ * main - Print all single digits of base 10
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
