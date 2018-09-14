e82aadca23d15ee4ad4c7fcffc9af5dc55d47dc3
jozsa <allisonjoyweiner@gmail.com>
2018-09-14 01:42:21 +0000

    Printing the alphabet except some letters

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,26 @@
 #include <stdio.h>
+
+/**
+ * main - Print the alphabet except q and e
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+
+{
+	char z = 'a';
+
+	do {
+		if ((z == 'e') || (z == 'q'))
+		{
+			z++;
+			continue;
+		}
+		putchar(z);
+		z++;
+	} while (z <= 'z');
+	putchar('\n');
+	return (0);
+}
