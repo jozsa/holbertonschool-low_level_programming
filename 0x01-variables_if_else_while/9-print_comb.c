4f6de3311db696a4b75c7e4c9e6bc865dcd8dec8
jozsa <allisonjoyweiner@gmail.com>
2018-09-17 19:17:07 +0000

    Printing combos of digits using while loops

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,18 +11,18 @@ int main(void)
 {
 	int n = 0;
 
-	while (n <= 9)
+	while (n < 10)
 	{
-		if (n < 9)
-		putchar(n + '0');
+		putchar('0' + n);
 		putchar(',');
 		putchar(' ');
 		n++;
 		if (n == 9)
 		{
-			putchar(n + '0');
+			putchar('0' + n);
 			break;
 		}
 	}
+	putchar('\n');
 	return (0);
 }
