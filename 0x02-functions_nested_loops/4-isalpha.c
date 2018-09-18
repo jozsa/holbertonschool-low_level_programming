0b06ded35a6e0ee21d8225e61f6594d80ddd31e5
jozsa <allisonjoyweiner@gmail.com>
2018-09-18 04:29:58 +0000

    Testing alphabetic letters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _isalpha - checks for alphabetic characters
+ * @c: Variable containing the letter to be checked
+ *
+ * Return: 1 if c is a letter, 0 otherwise
+ *
+ */
+
+int _isalpha(int c)
+{
+        int letter = c;
+
+        if ((letter < 123 && letter > 96) || (letter < 90 && letter > 65))
+                return (1);
+        else
+                return (0);
+}
