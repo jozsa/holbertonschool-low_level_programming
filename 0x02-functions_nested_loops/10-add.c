7050add680d133bf51f7fb79642975452840a03c
jozsa <allisonjoyweiner@gmail.com>
2018-09-18 21:06:41 +0000

    Adding numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * add - Add two integers
+ * @a: The first integer to be added
+ * @b: The second integer to be added
+ *
+ * Return: the result of the addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
