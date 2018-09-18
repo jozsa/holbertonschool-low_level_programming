f728de0fdd73db4c170133fafa6a7767b1f6a425
jozsa <allisonjoyweiner@gmail.com>
2018-09-18 19:52:18 +0000

    Printing every minute of a day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * jack_bauer - Prints every minute of a day
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hr = 0;
+	int min = 0;
+
+	while (hr < 24)
+	{
+		_putchar((hr/10) + '0');
+		_putchar((hr%10) + '0');
+		_putchar(':');
+		_putchar((min/10) + '0');
+		_putchar((min%10) + '0');
+		if (hr == 23 && min == 59)
+			break;
+		_putchar('\n');
+		min++;
+		if (min > 59)
+		{
+			hr++;
+			min = 0;
+		}
+	}
+	putchar('\n');
+}
