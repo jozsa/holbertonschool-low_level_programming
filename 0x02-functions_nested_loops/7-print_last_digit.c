d1d9a33c3720193434edfbbd56fa8e3771957688
jozsa <allisonjoyweiner@gmail.com>
2018-09-19 00:00:27 +0000

    Betty style hotfix 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,16 +3,17 @@
 
 /**
  * print_last_digit - Prints the last digit of a number
+ * @number: The number to be checked
  *
  * Return: The last digit of the number
  */
 
 int print_last_digit(int number)
 {
-        if (number < 0)
+	if (number < 0)
 		number = number * -1;
-	
+
 	_putchar((number % 10) + '0');
 
-        return (number % 10);
+	return (number % 10);
 }
