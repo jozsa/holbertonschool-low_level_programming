ab01787abb60367f301fb0e1d853e8b7ecafee80
jozsa <allisonjoyweiner@gmail.com>
2018-09-24 19:05:57 +0000

    Dereferencing pointers

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Updates value pointer points to to 98
+ * @n: The pointer to be updated
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
