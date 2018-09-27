39ec2014c9124acdf540a3bbf8d640cd4e90eb1b
jozsa <allisonjoyweiner@gmail.com>
2018-09-27 17:47:31 +0000

    Copying strings

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strcpy - Copy a string from src to dest
+ * @dest: The pointer to the buffer zone for the string to be copied
+ * @src: The pointer to the string to be copied
+ *
+ * Return: The pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *storage = dest;
+
+	while (*src)
+	{
+		*dest++ = *src++;
+	}
+	*dest = 0;
+	return (storage);
+}
