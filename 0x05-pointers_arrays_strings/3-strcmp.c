ac8fdb1837f50cedd754854500ed022f67d3a84f
jozsa <allisonjoyweiner@gmail.com>
2018-09-28 03:36:09 +0000

    Comparing two strings

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,26 @@
 #include "holberton.h"
 
+/**
+ * _strcmp - Compares two strings
+ * @s1: The first string to be compared
+ * @s2: The second string to be compared
+ *
+ * Return: -15 if s1 < s2, 15 if s1 > s2, 0 if they are equal
+ */
+
 int _strcmp(char *s1, char *s2)
 {
 	for ( ; *s1 == *s2; ++s1, ++s2)
+	{
 		if (*s1 == 0)
-			return 0;
-	return s1 < s2
-		? -15 : 15;
+			return (0);
+	}
+
+	if (s1 < s2)
+	{
+		return (-15);
+	}
+
+	else
+		return (15);
 }
