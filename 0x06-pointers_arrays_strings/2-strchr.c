1633cb66e1bafe0bc600934b7194506ab4800b51
jozsa <allisonjoyweiner@gmail.com>
2018-10-01 22:41:29 +0000

    Creating my own strchr function

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -15,10 +15,8 @@ char *_strchr(char *s, char c)
 		if (*s == c)
 			return (s);
 		else if (*s == '\0')
-			return (s);
-		else if (c == '\0')
-			return (0);
+			return ('\0');
 		s++;
 	}
-	return (0);
+	return (s);
 }
