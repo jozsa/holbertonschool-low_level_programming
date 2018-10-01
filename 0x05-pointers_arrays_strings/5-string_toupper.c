00cfdb231c9175c6926612701a7dc1f3f8633fce
jozsa <allisonjoyweiner@gmail.com>
2018-10-01 00:27:56 +0000

    Converting characters in a string to uppercase

--- 0x05-pointers_arrays_strings/5-string_toupper.c
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -4,15 +4,18 @@
  * string_toupper - Convert a string to uppercase
  * @str: The string to be converted
  *
+ * Return: The string
  */
 
 char *string_toupper(char *str)
 {
-	while (*str)
+	int num = 0;
+
+	while (str[num])
 	{
-		if (*str >= 97 && *str <= 122)
-			*str = *str - 32;
-		str++;
+		if (str[num] >= 97 && str[num] <= 122)
+			str[num] = str[num] - 32;
+		num++;
 	}
 	return (str);
 }
