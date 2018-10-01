de1ed8774468a1c9a59f100725538c4cca131ab0
jozsa <allisonjoyweiner@gmail.com>
2018-10-01 22:12:05 +0000

    Creating my own strchr function

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strchr: Locate a character in a string
+ * @s: The string to be evaluated
+ * @c: The character to be located
+ *
+ * Return: A pointer to s if c is found, NULL otherwise
+ */
+
+char *_strchr(char *s, char c)
+{
+	while(*s++)
+	{
+		if (*s == c)
+			return (s);
+		if (*s == '\0')
+			return (s);
+	}
+	return(0);
+}
