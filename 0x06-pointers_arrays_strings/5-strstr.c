fbbc1f35982ee5911a8e35a8ee632a5bad9a7ed5
jozsa <allisonjoyweiner@gmail.com>
2018-10-02 04:29:38 +0000

    Creating my own strstr function

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strstr - Finds the first occurence of a substring in a string
+ * @haystack: The string to be searched
+ * @needle: The substring to be found
+ *
+ * Return: Pointer to the beginning of the located substring, NULL if not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	if (needle[0] == '\0')
+		return (haystack);
+	while (*haystack)
+	{
+		if (*haystack == *needle)
+			return (haystack);
+		haystack++;
+	}
+	if (*haystack != *needle)
+		return (0);
+	return (haystack);
+}
