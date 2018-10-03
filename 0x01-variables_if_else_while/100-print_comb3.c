0e17b2240333e8d9591efe5e08b83360d2a22d5a
jozsa <allisonjoyweiner@gmail.com>
2018-10-03 05:32:58 +0000

    Printing all permutations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int digit_1 = 0, digit_2 = 0;
+
+	for (digit_1 = 0; digit_1 <= 9; digit_1++)
+	{
+		for(digit_2 = 0; digit_2 <= 9; digit_2++)
+		{
+			if (digit_1 != digit_2 && digit_1 < digit_2)
+			{
+				putchar(digit_1 + '0');
+				putchar(digit_2 + '0');
+				if (digit_1 < 8 && digit_2 <= 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
