a3d85f6f61378a3561fbcbee2ef518158e8fd4d8
jozsa <allisonjoyweiner@gmail.com>
2018-10-04 02:19:14 +0000

    Finding the square root of numbers

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -2,10 +2,11 @@
 #include "holberton.h"
 
 /**
- * _sqrt_recursion - Finds the natural square root of a number
- * @n: The number to find the natural square root of
+ * _sqrt_counter - Tests x to see if it is the square root of y
+ * @x: The number to test against y, increment if it doesnt pass
+ * @y: The squared number to find the square root of
  *
- * Return: -1 if no natural square root, else the square root of n
+ * Return: -1 if no natural square root, x otherwise
  */
 
 int _sqrt_counter(int x, int y)
@@ -17,12 +18,19 @@ int _sqrt_counter(int x, int y)
 		return (x);
 	}
 	else
-		return(_sqrt_counter(++x, y));
+		return (_sqrt_counter(++x, y));
 }
+
+/**
+ * _sqrt_recursion - Finds the natural square root of a number
+ * @n: The number to find the natural square root of
+ *
+ * Return: -1 if no natural square root, otherwise the square root of n
+ */
 
 int _sqrt_recursion(int n)
 {
 	if (n == 0)
 		return (-1);
-	return (_sqrt_counter (1, n));
+	return (_sqrt_counter(1, n));
 }
