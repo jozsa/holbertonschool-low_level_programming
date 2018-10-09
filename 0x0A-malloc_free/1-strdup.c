e0ebadfc0db8bbe4bc84210b78c5bbdf9dfd09ba
jozsa <allisonjoyweiner@gmail.com>
2018-10-09 23:04:38 +0000

    Allocating space for duplicated strings using malloc

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Duplicates a string in a newly allocated space in memory
+ * @str: The string to be duplicated
+ *
+ * Return: Pointer to the newly allocated space in memory
+ */
+
+char *_strdup(char *str)
+{
+	int length = 0;
+	char *str2;
+	char *storage;
+
+	while (str[length])
+		length++;
+
+	str2 = (char *)malloc(sizeof(char) * (length + 1));
+	storage = str2;
+
+	while (*str)
+		*storage++ = *str++;
+
+	*storage = '\0';
+
+	return (str2);
+}
