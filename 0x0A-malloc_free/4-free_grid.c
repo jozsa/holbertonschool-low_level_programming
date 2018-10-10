a98ace8f8e9ebcbe906e42f3b4864c1c22b5f2a9
jozsa <allisonjoyweiner@gmail.com>
2018-10-10 06:37:00 +0000

    Freeing up space using malloc

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees a previously created 2D array
+ * @grid: The grid to be freed
+ * @height: The height of the grid to be freed
+ */
+
+void free_grid(int **grid, int height)
+{
+	while(*grid)
+	{
+		while(height)
+		{
+			free(grid[height]);
+			height--;
+		}
+		free(grid);
+		grid++;
+	}
+}
