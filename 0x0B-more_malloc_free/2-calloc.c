dd4e87c69e865657175796465fc85adc3cbe3250
jozsa <allisonjoyweiner@gmail.com>
2018-10-12 03:39:40 +0000

    Allocating space for an array

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocate memory for an array
+ * @nmemb: Number of elements in array
+ * @size: Size, in bytes, for each element
+ *
+ * Return: A pointer to the allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arr;
+	unsigned int index;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(size * nmemb);
+	if (arr == NULL)
+		return (NULL);
+	while (index <= nmemb)
+	{
+		arr[index] = 0;
+		index++;
+	}
+	return (arr);
+}
