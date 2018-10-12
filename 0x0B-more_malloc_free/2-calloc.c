16a49495fd77c456905ac71a8123bf589353e5e0
jozsa <allisonjoyweiner@gmail.com>
2018-10-12 04:31:56 +0000

    Fix: allocate 1 less memory

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	arr = malloc(size * (nmemb + 1));
+	arr = malloc(size * nmemb);
 	if (arr == NULL)
 		return (NULL);
 	ptr = arr;
