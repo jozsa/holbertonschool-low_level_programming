87e47226753311c93fbfeba3496ff375bd7899ae
jozsa <allisonjoyweiner@gmail.com>
2018-10-12 05:15:53 +0000

    Incrementing index as well as num

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -6,18 +6,21 @@ int *array_range(int min, int max)
 {
 	int *arr;
 	int index = 0;
-	int num = 0;
+	int num;
 
 	if (min > max)
 		return (NULL);
 
 	arr = malloc(sizeof(int) * ((max - min) + 1));
+
 	if (arr == NULL)
 		return (NULL);
+
 	num = min;
-	while (index <= (max - min))
+	while (index <= ((max - min) + 1))
 	{
 		arr[index] = num;
+		index++;
 		num++;
 	}
 	return(arr);
