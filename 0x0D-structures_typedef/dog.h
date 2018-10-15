a10c98bede3742a3450eee120997945225798087
jozsa <allisonjoyweiner@gmail.com>
2018-10-15 21:52:04 +0000

    Defined a new type named 'dog' using struct

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Descriptors for dogs
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
