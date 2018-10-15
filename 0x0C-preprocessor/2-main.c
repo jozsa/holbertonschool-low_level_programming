06ebbac32f4d7ba1d1ec66d4a9af08858f83f067
jozsa <allisonjoyweiner@gmail.com>
2018-10-15 19:43:08 +0000

    Using __FILE__ macro to print file name

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the name of the file it was compiled from
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
