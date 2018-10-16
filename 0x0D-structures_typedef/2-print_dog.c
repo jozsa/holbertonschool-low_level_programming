86a1622edac3513eedc029b8b44f6ded5eba3e15
jozsa <allisonjoyweiner@gmail.com>
2018-10-16 00:05:19 +0000

    Edited NULL check to check floats too

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d->name == NULL)
+	if (!d->name)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
@@ -19,7 +19,7 @@ void print_dog(struct dog *d)
 		printf("Age: (nil)\n");
 	else
 		printf("Age: %f\n", d->age);
-	if (d->owner == NULL)
+	if (!d->owner)
 		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
