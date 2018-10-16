df2d3621b6d00ce42487d15d2e9f532cca47aaf3
jozsa <allisonjoyweiner@gmail.com>
2018-10-16 01:27:53 +0000

    Creating a new dog

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,16 +1,32 @@
 #include "dog.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - Creates a new dog of type struct dog
+ * @name: The new dog's name
+ * @age: The new dog's age
+ * @owner: The new dog's owner
+ *
+ * Return: A pointer to the new dog
+ */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggo;
 
 	doggo = malloc(sizeof(struct dog));
-
 	if (doggo == NULL)
 		return (NULL);
-
+	doggo->name = malloc(sizeof(name));
+	if (doggo->name == NULL)
+		return (NULL);
+	doggo->owner = malloc(sizeof(owner));
+	if (doggo->owner == NULL)
+	{
+		free(doggo->name);
+		return (NULL);
+	}
 	doggo->name = name;
 	doggo->age = age;
 	doggo->owner = owner;
