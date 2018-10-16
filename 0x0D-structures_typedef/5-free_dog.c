7c6d32ac9986daf8592faea47404bd2bad4a8023
jozsa <allisonjoyweiner@gmail.com>
2018-10-16 02:34:46 +0000

    Freeing all the dogs

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - Free the memory the dog is stored in
+ * @d: The memory to be freed
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
