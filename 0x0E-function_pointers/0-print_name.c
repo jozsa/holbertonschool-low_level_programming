6ee0903201c62c3459dd0d2bf64c722d5bcc7ebe
jozsa <allisonjoyweiner@gmail.com>
2018-10-17 19:57:27 +0000

    Created case for if name is NULL

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+ * print_name - Print a name
+ * @name: The name passed to the function
+ * @f: The pointer to other functions used to print a name
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL)
+		f(name);
 }
