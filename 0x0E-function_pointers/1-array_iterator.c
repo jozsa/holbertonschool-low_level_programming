bd588e64e1c2ce9f23f70b67e2e6ce5496686594
jozsa <allisonjoyweiner@gmail.com>
2018-10-17 20:34:37 +0000

    Betty compliance fixes

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -2,10 +2,14 @@
 #include "function_pointers.h"
 
 /**
+ * array_iterator - Execute functions on elements of an array
+ * @array: The array to iterate through
+ * @size: The size of the array
+ * @action: Pointer to functions to be executed
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
-{	
+{
 	unsigned int index = 0;
 
 	if (array == NULL || action == NULL)
