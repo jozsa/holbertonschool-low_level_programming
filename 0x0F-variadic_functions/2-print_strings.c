197bef36744aaad493c82579aa288da4dc8c5a66
jozsa <allisonjoyweiner@gmail.com>
2018-10-21 20:20:30 +0000

    Edited typo for printing nil

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(ap, char*);
 		if (str == NULL)
-			printf("(nil)");
+			str = "(nil)";
 		printf("%s", str);
 		if (separator != NULL && index != (n - 1))
 			printf("%s", separator);
