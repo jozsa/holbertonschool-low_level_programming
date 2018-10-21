d718027ed930733b5e79a79f16edafe4848699d1
jozsa <allisonjoyweiner@gmail.com>
2018-10-21 03:42:00 +0000

    Updated header file

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,6 +1,12 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+typedef struct types
+{
+	char *type;
+	void (*f)();
+} test;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
