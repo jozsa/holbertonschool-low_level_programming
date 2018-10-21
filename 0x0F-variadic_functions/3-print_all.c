0140949533f57f81b2f57cf73b12498200f651c9
jozsa <allisonjoyweiner@gmail.com>
2018-10-21 04:49:18 +0000

    Edited typo in NULL checker

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -3,20 +3,40 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * print_c - Prints a char to the standard output
+ * @ap: The argument(s) passed to the function
+ */
+
 void print_c(va_list ap)
 {
 	printf("%c", va_arg(ap, int));
 }
+
+/**
+ * print_i - Prints an integer to the standard output
+ * @ap: The argument(s) passed to the function
+ */
 
 void print_i(va_list ap)
 {
 	printf("%d", va_arg(ap, int));
 }
 
+/**
+ * print_f - Prints a floating point number to the standard output
+ * @ap: The argument(s) passed to the function
+ */
+
 void print_f(va_list ap)
 {
 	printf("%f", va_arg(ap, double));
 }
+
+/**
+ * print_s - Prints a string to the standard output
+ * @ap: The argument(s) passed to the function
+ */
 
 void print_s(va_list ap)
 {
@@ -33,6 +53,7 @@ void print_all(const char * const format, ...)
 	va_list ap;
 	int index = 0, index2 = 0;
 	int count = 0;
+	char *sep = "";
 	test printer[5] = {
 		{"c", print_c},
 		{"i", print_i},
@@ -50,9 +71,9 @@ void print_all(const char * const format, ...)
 		{
 			if (format[index2] == *(printer[index].type))
 			{
+				printf("%s", sep);
 				printer[index].f(ap);
-				if (count < 3)
-					printf(", ");
+				sep = ", ";
 			}
 			index++;
 		}
