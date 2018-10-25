c02548eb826ae35ebc347ce93972dc14fba0e5ed
jozsa <allisonjoyweiner@gmail.com>
2018-10-25 23:56:50 +0000

    NULL check fixes

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -15,8 +15,6 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new, *last;
 
 	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
 	for (length = 1; str && str[length]; length++)
 		;
 	last = *head;
@@ -32,5 +30,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	while (last->next != NULL)
 		last = last->next;
 	last->next = new;
+	if (new == NULL)
+		return (NULL);
 	return (new);
 }
