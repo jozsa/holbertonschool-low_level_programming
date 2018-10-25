b56fc44b266043b541bcfaf51ac142c35d361758
jozsa <allisonjoyweiner@gmail.com>
2018-10-25 20:44:07 +0000

    Betty-style fixes

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -2,10 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * print_list - Prints the elements of a singly linked list of type list_t
+ * @h: A pointer to each list
+ *
+ * Return: The number of nodes in each list
+ */
+
 size_t print_list(const list_t *h)
 {
 	int count = 0;
-	
+
 	while (h)
 	{
 		if (h->str == NULL)
