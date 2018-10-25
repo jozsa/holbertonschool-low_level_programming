3e96a66b4cfa9ae49222496faf52e0285983fe82
jozsa <allisonjoyweiner@gmail.com>
2018-10-25 20:49:42 +0000

    Betty style fixes for header file

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdlib.h>
 
 /**
@@ -20,3 +23,5 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+
+#endif /* LISTS_H */
