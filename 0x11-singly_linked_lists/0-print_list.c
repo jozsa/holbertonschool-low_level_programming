a724fc66c43bf4f252b4bc46899eba4554ccf814
jozsa <allisonjoyweiner@gmail.com>
2018-10-25 20:41:32 +0000

    Printing the elements of a singly linked list

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
