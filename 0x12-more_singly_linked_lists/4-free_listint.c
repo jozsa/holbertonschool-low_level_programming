c3acea9e40449f40adaeabe5d0682a775248d63a
jozsa <allisonjoyweiner@gmail.com>
2018-10-29 21:44:19 +0000

    Removed redundant frees

--- 0x12-more_singly_linked_lists/4-free_listint.c
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -9,11 +9,11 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
+	temp = head;
 	while (head != NULL)
 	{
-		temp = head;
 		free(head);
 		head = head->next;
-		free(temp);
 	}
+	free(temp);
 }
