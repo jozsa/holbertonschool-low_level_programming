c7884a25cd5d603eb8289da9b66856ab4284c6e8
jozsa <allisonjoyweiner@gmail.com>
2018-10-29 21:52:48 +0000

    Betty style fixes

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * free_listint - Free all the elements of a linked list
+ * free_listint2 - Free all the elements of a linked list
  * @head: A pointer to the linked list
  */
 
 void free_listint2(listint_t **head)
 {
-        listint_t *temp;
+	listint_t *temp;
 
 	temp = *head;
-        while (temp != NULL)
-        {
+	while (temp != NULL)
+	{
 		temp = temp->next;
-                free(temp);
-        }
+		free(temp);
+	}
 	free(*head);
-        *head = NULL;
+	*head = NULL;
 }
