67f525be60615db9277297114b2b13550476b308
jozsa <allisonjoyweiner@gmail.com>
2018-10-29 20:08:49 +0000

    Betty-style fixes

--- 0x12-more_singly_linked_lists/2-add_nodeint.c
+++ 0x12-more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * add_nodeint - Adds a node to the beginning of a linked list
+ * @head: A pointer to a pointer to a linked list
+ * @n: The data/integer to be added to each node
+ *
+ * Return: A pointer to the address of the new node
+ */
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
