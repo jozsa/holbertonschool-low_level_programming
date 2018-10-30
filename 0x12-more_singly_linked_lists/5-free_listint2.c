9ad7d3f7df7dc31cb8da363fff21e385c055e62e
jozsa <allisonjoyweiner@gmail.com>
2018-10-30 20:16:48 +0000

    Retyping code to see if there is an issue with the checker

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head != NULL)
-	{
-		temp = *head;
+	temp = *head;
+	while (temp != NULL)
+	{		
 		free(temp);
-		*head = (*head)->next;
+		temp = temp->next;
 	}
-	head = NULL;
+	*head = NULL;
 }
