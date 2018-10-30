fe702ead79fe8c1a3c759c45f758628aff0c5055
jozsa <allisonjoyweiner@gmail.com>
2018-10-30 18:36:41 +0000

    Return NULL if head is NULL

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *current = *head;
 	unsigned int count;
 
-	if (new == NULL)
+	if (new == NULL || *head == NULL)
 		return (NULL);
 	new->n = n;
 	for (count = 0; count < (idx - 1); count++)
