d125ed14a917d6b721730d86a03842a181467c25
jozsa <allisonjoyweiner@gmail.com>
2018-11-01 22:36:22 +0000

    Betty-style fixes and variable name edits

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -2,23 +2,26 @@
 #include <stdio.h>
 
 /**
+ * binary_to_uint - Converts a binary number to an unsigned integer
+ * @b: The string containing the binary number
  *
+ * Return: The unsigned int to return
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int dec;
+	unsigned int num;
 
 	if (b == NULL)
 		return (0);
-	for (dec = 0; *b; b++)
+	for (num = 0; *b; b++)
 	{
 		if (*b == '1')
-			dec = (dec << 1) | 1;
+			num = (num << 1) | 1;
 		else if (*b == '0')
-			dec <<= 1;
+			num <<= 1;
 		else
 			return (0);
 	}
-	return (dec);
+	return (num);
 }
