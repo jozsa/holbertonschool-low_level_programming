ff08c3c3070ccda45259908c784635b2ce09b438
jozsa <allisonjoyweiner@gmail.com>
2018-11-02 01:07:25 +0000

    Fixed error condition

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -11,18 +11,12 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int mask, masked, th;
-	unsigned int count;
 
 	mask = 1 << index;
 	masked = n & mask;
 	th = masked >> index;
 
-	for (count = 0; n != 0; n >>= 1)
-	{
-		if (n & 01)
-			count++;
-	}
-	if (index > count)
+	if (index > 31)
 		return (-1);
 	return (th);
 }
