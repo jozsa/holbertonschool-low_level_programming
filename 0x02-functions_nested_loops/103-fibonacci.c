620ea5e2eb4e264d17956ad340d4ba1e488bd322
jozsa <allisonjoyweiner@gmail.com>
2018-11-04 20:52:43 +0000

    Adding all even fibonacci numbers up to 4M

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Sum even Fibonacci numbers under 4,000,000
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	unsigned long int num1 = 2, num2 = 3, sum = 2, temp = 0;
+
+	while (temp < 4000000)
+	{
+		temp = num1 + num2;
+		if (temp % 2 == 0)
+		{
+			sum = sum + temp;
+			printf("%lu\n", sum);
+		}
+		num1 = num2;
+		num2 = temp;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
