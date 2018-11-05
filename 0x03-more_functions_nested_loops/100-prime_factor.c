4e3085e3a635b49af325734a779a765bea756edc
jozsa <allisonjoyweiner@gmail.com>
2018-11-05 03:34:56 +0000

    Finding the largest prime factor of 612852475143

--- 0x03-more_functions_nested_loops/100-prime_factor.c
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 /**
  * main - Prints largest prime factor of 612852475143
@@ -11,19 +12,19 @@ int main(void)
 	unsigned long int x = 612852475143, quotient = 0, primetester = 0;
 	unsigned int modulo, divisor;
 
-	for (divisor = 2; divisor < x; divisor++)
+	for (divisor = 2; divisor < sqrt(x); divisor++)
 	{
 		if (x % divisor == 0)
 		{
 			quotient = x / divisor;
-			for (modulo = 2; modulo < 10; modulo++)
+			for (modulo = 2; modulo < quotient; modulo++)
 			{
 				primetester = quotient % modulo;
 				if (primetester == 0)
 					break;
 			}
 		}
-		if (modulo == 10)
+		if (modulo == quotient)
 		{
 			printf("%lu\n", quotient);
 			break;
