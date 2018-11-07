d961cdcc5c510089e0acb014b3915e01f821de9b
jozsa <allisonjoyweiner@gmail.com>
2018-11-07 02:32:00 +0000

    Calling read/write beyond 1024 bytes

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <limits.h>
 #include "holberton.h"
 
 /**
@@ -14,7 +15,7 @@
 
 int main(int argc, char **argv)
 {
-	int length = 0, fd = 0, wt = 0, ct = 0;
+	int length = 0, fd = 0, fd2, wt = 0, ct = 0, ct2 = 0;
 	char *buffer;
 
 	if (argc != 3)
@@ -24,31 +25,31 @@ int main(int argc, char **argv)
 	}
 	buffer = malloc(1024);
 	fd = open(argv[1], O_RDONLY);
+	fd2 = open(argv[2], O_RDWR | O_TRUNC | O_CREAT, 0664);
 	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	length += read(fd, buffer, 1024);
-	ct = close(fd);
-	if (ct == -1)
+	length = read(fd, buffer, 1024);
+	wt = write(fd2, buffer, length);
+	while (length != 0)
 	{
-		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd);
-		exit(100);
+		length = read(fd, buffer, 1024);
+		wt = write(fd2, buffer, length);
+		buffer = buffer + length;
 	}
-	fd = open(argv[2], O_RDWR | O_TRUNC | O_CREAT, 0664);
-	wt = write(fd, buffer, length);
-	if (fd == -1 || wt == -1)
+	if (fd2 == -1 || wt == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-		exit(99);
-	}
+                exit(99);
+        }
 	ct = close(fd);
-	if (ct == -1)
+	ct2 = close(fd2);
+	if (ct == -1 || ct2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd);
 		exit(100);
 	}
-	free(buffer);
 	return (0);
 }
