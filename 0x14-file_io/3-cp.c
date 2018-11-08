f45f1b3407e22b15cc9130788af074d66330f590
jozsa <allisonjoyweiner@gmail.com>
2018-11-08 03:27:32 +0000

    Wrote helper functions and fixed buffer loop

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -5,6 +5,48 @@
 #include "holberton.h"
 
 /**
+ * error_97 - Prints error message and exits for incorrect number of arguments
+ */
+
+void error_97(void)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+
+/**
+ * error_98 - Prints error message and exits for read failure
+ * @filename: The file that didn't exist or couldn't be read
+ */
+
+void error_98(char *filename)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+	exit(98);
+}
+
+/**
+ * error_99 - Prints error message and exits for write failure
+ * @filename: File that couldn't be created or couldnt be written to
+ */
+
+void error_99(char *filename)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+	exit(99);
+}
+
+/**
+ * error_100 - Prints error message and exits for close failure
+ * @fd: The file descriptor that couldn't be closed
+ */
+
+void error_100(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
+}
+/**
  * main - Copies the content of a file to another file
  * @argc: The number of arguments provided to the function
  * @argv: The pointer to each argument provided to the function
@@ -18,40 +60,29 @@ int main(int argc, char **argv)
 	char *buffer;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		error_97();
 	buffer = malloc(1024);
 	if (buffer == NULL)
 		return (0);
-	fd = open(argv[1], O_RDONLY);
+	fd = open(argv[1], O_RDWR);
+	if (fd == -1)
+		error_98(argv[1]);
 	fd2 = open(argv[2], O_RDWR | O_TRUNC | O_CREAT, 0664);
+	if (fd2 == -1)
+		error_99(argv[2]);
 	length = read(fd, buffer, 1024);
-	if (length == -1 || fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		free(buffer);
-		exit(98);
-	}
-	wt = write(fd2, buffer, length);
+	if (length == -1)
+		error_98(argv[1]);
 	while (length > 0)
 	{
-		if (wt == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			free(buffer);
-			exit(99);
-		}
-		length = read(fd, buffer, 1024);
 		wt = write(fd2, buffer, length);
-		buffer = buffer + length;
+		if (wt == -1)
+			error_99(argv[2]);
+		length = read(fd, buffer, 1024);
 	}
-	if (close(fd) == -1 || close(fd2) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		free(buffer);
-		exit(100);
-	}
+	if (close(fd) == -1)
+		error_100(fd);
+	if (close(fd2) == -1)
+		error_100(fd2);
 	return (0);
 }
