d522b91f55428275e59e103a83add6a138c3e7ee
jozsa <allisonjoyweiner@gmail.com>
2018-12-09 20:46:35 +0000

    Inserting a node at a given index in a doubly linked list

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,26 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - Return the number of elements in a doubly linked list
+ * @h: The first node of the doubly linked list
+ *
+ * Return: Number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp = h;
+	size_t count = 0;
+
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
+
+/**
   * insert_dnodeint_at_index - Insert a node at idx
   * @h: A pointer to the beginning of the doubly linked list
   * @idx: The index to insert the new node at
@@ -19,6 +39,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new == NULL || *h == NULL)
 		return (NULL);
 	new->n = n;
+	count = dlistint_len(*h);
+	if (idx > count)
+		return (NULL);
 	for (count = 0; count < (idx - 1); count++)
 		temp = temp->next;
 	new->next = temp->next;
