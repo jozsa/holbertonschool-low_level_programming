18ae0b638ef6ba725f819b329a854f5fbff55a2b
jozsa <allisonjoyweiner@gmail.com>
2019-01-15 04:19:37 +0000

    Getting the value associated with a specific key in a hash table

---
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Gets the value associated with a key of the hash table
+ * @ht: The hash table to search
+ * @key: The key to match
+ *
+ * Return: The value if found, NULL otherwise
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int index;
+	hash_node_t *temp;
+
+	index = key_index((unsigned char *) key, ht->size);
+	temp = ht->array[index];
+	if (!temp)
+		return (NULL);
+	if (strcmp(temp->key, key) == 0)
+		return (temp->value);
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
+}
