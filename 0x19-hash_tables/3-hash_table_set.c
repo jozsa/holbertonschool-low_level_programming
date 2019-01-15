e12266e37818bf556c436a2bc623bec9c08df037
jozsa <allisonjoyweiner@gmail.com>
2019-01-15 04:19:18 +0000

    Adding elements to hash table

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Insert an element to the hash table
+ * @ht: Pointer to the hash table to add element to
+ * @key: The key to add to the hash table
+ * @value: Value associated with the key to add
+ *
+ * Return: 1 upon success, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	size_t index;
+	hash_node_t *new;
+	char *key_dup = strdup(key);
+
+	if (strcmp(key, "") == 0)
+		return (0);
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	index = key_index((unsigned char *)key_dup, ht->size);
+	new->key = key_dup;
+	new->value = strdup(value);
+	if (ht->array[index] == NULL)
+	{
+		new->next = NULL;
+		ht->array[index] = new;
+		return (1);
+	}
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
+}
