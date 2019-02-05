d9df54c3bf9f915afaa8c3099c8979c1c201a6ec
jozsa <allisonjoyweiner@gmail.com>
2019-02-05 20:57:57 +0000

    Prototype header for counting sort

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -27,5 +27,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif /* _SORT_H_ */
