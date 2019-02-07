39255f9de2f2fb4a3b5c9f92410794644d6feff2
jozsa <allisonjoyweiner@gmail.com>
2019-02-07 21:13:45 +0000

    More readable code, changed variable names and added comments

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -8,30 +8,39 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i = 0;
-	int temp;
-	unsigned int flag = 0;
+	unsigned int i = 0;		/* index of array */
+	int temp;			/* storage variable for swapping */
+	unsigned int swapped = 0;	/* set to 1 if 2 elements have been swapped */
 
 	if (!array)
 		return;
 	while (i < size - 1)
 	{
+		/* if two elements are not in ascending order, swap them */
 		if (array[i] > array[i + 1])
 		{
 			temp = array[i];
 			array[i] = array[i + 1];
 			array[i + 1] = temp;
-			flag = 1;
+			swapped = 1;
 			print_array(array, size);
 		}
 		i++;
-		if (i == size - 1 && flag == 0)
+		/*
+		 * if a swap has not occured and the array is
+		 * at the end, the array has been sorted.
+		 */
+		if (i == size - 1 && !swapped)
 			return;
-		else if (i == size - 1 && flag == 1)
+		/*
+		 * if a swap has occured and the array is
+		 * at the end, go through the array again
+		 * and swap if necessary.
+		 */
+		else if (i == size - 1 && swapped)
 		{
 			i = 0;
-			flag = 0;
-			continue;
+			swapped = 0;
 		}
 	}
 }
