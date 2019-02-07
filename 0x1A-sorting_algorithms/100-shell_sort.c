80ebe87bf78f2f82fd267c6c8549497d4339134d
jozsa <allisonjoyweiner@gmail.com>
2019-02-07 21:26:09 +0000

    Variable name changed to interval from h for readability

--- 0x1A-sorting_algorithms/100-shell_sort.c
+++ 0x1A-sorting_algorithms/100-shell_sort.c
@@ -1,37 +1,37 @@
 #include "sort.h"
 
 /**
- * shell_sort - Shell sort an array using Knuth sequence
+ * sintervalell_sort - Sintervalell sort an array using Knutinterval sequence
  * @array: Array to sort
  * @size: Size of array to sort
  */
 void shell_sort(int *array, size_t size)
 {
-	unsigned int h = 1;	/* interval size */
+	unsigned int interval = 1;	/* interval size */
 	unsigned int i;		/* outer loop/leading variable */
 	unsigned int j;		/* inner loop/trailing variable */
 	int temp = 0;		/* temp variable to swap ints */
 
 	if (!array || size < 2)
 		return;
-	while (h * 3 + 1 < size)
+	while (interval * 3 + 1 < size)
 		/* calculate interval size based on size of array */
-		h = h * 3 + 1;
-	while (h > 0)
+		interval = interval * 3 + 1;
+	while (interval > 0)
 	{
-		for (i = h; i < size; i++)
+		for (i = interval; i < size; i++)
 		{
-			for (j = i; j > h - 1; j -= h)
+			for (j = i; j > interval - 1; j -= interval)
 			{
-				if (array[j - h] >= array[j])
+				if (array[j - interval] >= array[j])
 				{
 					temp = array[j];
-					array[j] = array[j - h];
-					array[j - h] = temp;
+					array[j] = array[j - interval];
+					array[j - interval] = temp;
 				}
 			}
 		}
-		h = (h - 1) / 3; /* decrement interval size */
+		interval = (interval - 1) / 3; /* decrement interval size */
 		print_array(array, size);
 	}
 }
