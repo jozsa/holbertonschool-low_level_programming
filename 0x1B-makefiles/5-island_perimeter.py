21ee89d4c891e541d1565e76d9d858d3df9d8550
jozsa <allisonjoyweiner@gmail.com>
2019-02-11 22:12:23 +0000

    Finding the perimeter of an island

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""island_perimeter(grid)
+
+One function that finds the perimeter of a given island within a grid
+A grid is a list of lists of integers, 0 = water, 1 = land
+"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island in the grid
+    """
+    perimeter = 0
+    for row in range(len(grid)):
+        for column in range(len(grid[row])):
+            if grid[row][column]:
+                perimeter += 4
+                try:
+                    if grid[row - 1][column]:
+                        perimeter -= 1
+                except IndexError:
+                    perimeter = perimeter
+                try:
+                    if grid[row][column - 1]:
+                        perimeter -= 1
+                except IndexError:
+                    perimeter = perimeter
+                try:
+                    if grid[row + 1][column]:
+                        perimeter -= 1
+                except IndexError:
+                    perimeter = perimeter
+                try:
+                    if grid[row][column + 1]:
+                        perimeter -= 1
+                except IndexError:
+                    perimeter = perimeter
+    return perimeter
