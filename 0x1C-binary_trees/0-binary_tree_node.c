ac711d198f8132d43b5f52c1e3a4928cfcc48d79
jozsa <allisonjoyweiner@gmail.com>
2019-03-11 23:59:56 +0000

    Betty compliance

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - Creates a new binary tree node
+ * @parent: Parent of new node
+ * @value: Value inside new node
+ *
+ * Return: New node
+ */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
