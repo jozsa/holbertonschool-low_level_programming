18c4a7aea69f56744354e3409fd9a33f2e17437f
jozsa <allisonjoyweiner@gmail.com>
2019-03-12 23:38:40 +0000

    Refactor, removed unnecessary += for depth

--- 0x1C-binary_trees/10-binary_tree_depth.c
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -15,6 +15,6 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 		return (0);
 	if (!tree->parent)
 		return (0);
-	depth += binary_tree_depth(tree->parent);
+	depth = binary_tree_depth(tree->parent);
 	return (depth + 1);
 }
