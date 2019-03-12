0810893a6745d2b37ea7c38fc915f5c30f017149
Rmolimock <495@holbertonschool.com>
2019-03-12 00:29:15 +0000

    prototype for insert left

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -26,6 +26,7 @@ typedef struct binary_tree_s binary_tree_t;
 /* creation & insertion */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
 /* deletion */
