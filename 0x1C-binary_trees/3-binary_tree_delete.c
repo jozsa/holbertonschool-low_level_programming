d2ba6e0c5f7062a6d1190b0325b42aac67af2078
Rmolimock <495@holbertonschool.com>
2019-03-12 17:08:34 +0000

    simple recursive delete

--- 0x1C-binary_trees/3-binary_tree_delete.c
+++ 0x1C-binary_trees/3-binary_tree_delete.c
@@ -8,57 +8,10 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	binary_tree_t *tmp;
-
-	tmp = tree;
-	/*end condition for recursion*/
-	if ((tree->left == NULL) && (tree->right == NULL))
+	if (tree)	
 	{
-		if (tree->parent != NULL)
-		{
-			tmp = tree->parent;
-			free(tree);
-			if (tmp->left == NULL)
-				tree->right = NULL;
-			else
-				tree->left = NULL;
-			binary_tree_delete(tmp);
-		}
-		else
-			free(tree);
-	}
-	/*move left*/
-	else if (tree->left != NULL)
-	{
-		tree = tree->left;
-		if (tree->right != NULL)
-		{
-			tree = tree->right;
-			binary_tree_delete(tree);
-		}
-		else if (tree->left == NULL)
-		{
-			tree = tree->parent;
-			free(tree->left);
-			tree->left = NULL;
-			binary_tree_delete(tree);
-		}
-	}
-	/*move right*/
-	else if (tree->right != NULL)
-	{
-		tree = tree->right;
-		if (tree->left != NULL)
-		{
-			tree = tree->left;
-			binary_tree_delete(tree);
-		}
-		else if (tree->right == NULL)
-		{
-			tree = tree->parent;
-			free(tree->right);
-			tree->right = NULL;
-			binary_tree_delete(tree);
-		}
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
 	}
 }
