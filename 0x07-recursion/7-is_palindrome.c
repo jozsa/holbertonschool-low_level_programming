5e6d78585a5525dd472e07f14073ac04bec50398
jozsa <allisonjoyweiner@gmail.com>
2019-03-14 23:04:41 +0000

    Revised palindrome function to work in all cases

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * _strlen_recursion - Finds the length of the string to be tested
@@ -16,6 +17,24 @@ int _strlen_recursion(char *s)
 }
 
 /**
+ * palindrome_helper - Helper function for recursive palindrome algo
+ * @s: String to check
+ * @start: First char of string to check
+ * @end: Last char of string to check
+ *
+ * Return: 1 if str is palindrome, 0 otherwise
+ */
+
+int palindrome_helper(char *s, int start, int end)
+{
+	end -= 1;
+	if (s[start] != s[end])
+		return (0);
+	palindrome_helper(s, start++, end--);
+	return (1);
+}
+
+/**
  * is_palindrome - Tests whether a string is a palindrome
  * @s: The string to test
  *
@@ -25,22 +44,8 @@ int _strlen_recursion(char *s)
 int is_palindrome(char *s)
 {
 	int end = _strlen_recursion(s);
-	int begin = 0;
-	int storage;
 
-	if (*s == '\0')
-		return (1);
-
-	if (s[begin] == s[end])
-	{
-		storage = s[begin];
-		s[begin] = s[end - 1];
-		s[end - 1] = storage;
-		end--;
-		return (1);
-	}
-	if (s[begin] != s[end])
+	if (!palindrome_helper(s, 0, end))
 		return (0);
-	else
-		return (1);
+	return (1);
 }
