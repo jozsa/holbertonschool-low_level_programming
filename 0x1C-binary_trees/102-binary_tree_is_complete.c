ccd84a9b257658e4863cd8a39e461c6190753b53
jozsa <allisonjoyweiner@gmail.com>
2019-03-15 23:43:55 +0000

    Betty compliance

--- 0x1C-binary_trees/102-binary_tree_is_complete.c
+++ 0x1C-binary_trees/102-binary_tree_is_complete.c
@@ -1,11 +1,10 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_complete - Checks whether or not a binary tree is complete
+ * @tree: The tree to check
  *
- *
- *
- *
- *
+ * Return: 1 if it is complete, 0 if not
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
@@ -15,11 +14,14 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 		return (0);
 	if (!tree->left && !tree->right)
 		return (1);
-	if (!(binary_tree_is_complete(tree->left)) && (binary_tree_is_complete(tree->right)))
+	if (!(binary_tree_is_complete(tree->left)) &&
+			(binary_tree_is_complete(tree->right)))
 		return (0);
-	if ((binary_tree_is_complete(tree->left) && (binary_tree_is_complete(tree->right))))
+	if ((binary_tree_is_complete(tree->left) &&
+				(binary_tree_is_complete(tree->right))))
 		return (1);
-	if (binary_tree_is_complete(tree->left) && !(binary_tree_is_complete(tree->right)))
+	if (binary_tree_is_complete(tree->left) &&
+			!(binary_tree_is_complete(tree->right)))
 		return (1);
 	return (0);
 }
