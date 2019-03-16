b6510b50b5af7b620a7f18730fa44b55a7ee96ae
jozsa <allisonjoyweiner@gmail.com>
2019-03-16 03:45:24 +0000

    Fix return (NULL) error in levelorder function

--- 0x1C-binary_trees/101-binary_tree_levelorder.c
+++ 0x1C-binary_trees/101-binary_tree_levelorder.c
@@ -59,7 +59,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 	binary_tree_t *temp = NULL;
 
 	if (!tree || !func)
-		return (NULL);
+		return;
 	enqueue(&head, (binary_tree_t *)tree);
 	while (head)
 	{
