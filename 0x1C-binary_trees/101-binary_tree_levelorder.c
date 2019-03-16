fddc48abf1e7fc1abb8f118d4a43e49df85c7179
jozsa <allisonjoyweiner@gmail.com>
2019-03-16 02:33:10 +0000

    Level order traversal of binary tree

--- 0x1C-binary_trees/101-binary_tree_levelorder.c
+++ 0x1C-binary_trees/101-binary_tree_levelorder.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * enqueue - Implement linked  list queue for level order traversal
+ * @head: Head of queue
+ * @node: Node to add to queue
+ *
+ * Return: Pointer to new node added
+ */
 queue_t *enqueue(queue_t **head, binary_tree_t *node)
 {
 	queue_t *new, *last;
@@ -12,14 +19,21 @@ queue_t *enqueue(queue_t **head, binary_tree_t *node)
 	if (*head == NULL)
 	{
 		*head = new;
-		return (*head);
+		return (new);
 	}
+	last = *head;
 	while (last->next)
 		last = last->next;
 	last->next = new;
 	return (new);
 }
 
+/**
+ * dequeue - Pop a node from the queue to print it
+ * @head: Queue to pop
+ *
+ * Return: Pointer to popped node
+ */
 binary_tree_t *dequeue(queue_t **head)
 {
 	queue_t *temp = *head;
@@ -31,24 +45,29 @@ binary_tree_t *dequeue(queue_t **head)
 	*head = (*head)->next;
 	return (storage);
 }
+
+/**
+ * binary_tree_levelorder - Traverse binary tree using level order
+ * @tree: Tree to traverse
+ * @func: Pointer to function to execute while traversing
+ *
+ * Return: None
+ */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
 	queue_t *head = NULL;
 	binary_tree_t *temp = NULL;
-	(void)func;
 
+	if (!tree || !func)
+		return (NULL);
 	enqueue(&head, (binary_tree_t *)tree);
-	printf("tree->n is: %d\n", head->node->n);
-	temp = dequeue(&head);
-	func(temp->n);
-	if (head)
-		printf("node->n is: %d\n", head->node->n);
-	enqueue(&head, (binary_tree_t *)tree->left);
-	printf("tree->left->n is: %d\n", head->node->n);
-	temp = dequeue(&head);
-	func(temp->n);
-	enqueue(&head, (binary_tree_t *)tree->right);
-	printf("tree->right->n is: %d\n", head->node->n);
-	temp = dequeue(&head);
-	func(temp->n);
+	while (head)
+	{
+		temp = dequeue(&head);
+		func(temp->n);
+		if (temp->left)
+			enqueue(&head, (binary_tree_t *)temp->left);
+		if (temp->right)
+			enqueue(&head, (binary_tree_t *)temp->right);
+	}
 }
