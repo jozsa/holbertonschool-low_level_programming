62299716f2400539c0d87703d48802871c0109d6
jozsa <allisonjoyweiner@gmail.com>
2019-04-19 04:23:34 +0000

    Switched to working iterative solution:

--- 0x1D-search_algorithms/100-jump.c
+++ 0x1D-search_algorithms/100-jump.c
@@ -1,46 +1,6 @@
 #include "search_algos.h"
 #include <math.h>
 /**
- * j_s_helper - Provides low/high values for jump search
- * @array: Array to search
- * @low: First element of array to search
- * @high: Last element of array to search
- * @value: Element to search
- * @size: Size of array to search
- *
- * Return: Index of value if found, -1 if nonexistent array/not found
- */
-int j_s_helper(int *array, size_t low, size_t high, int value, size_t size)
-{
-	size_t index;
-	size_t i;
-
-	if (size == 0)
-	{
-		printf("Value found between indexes [0] and [0]\n");
-		return (-1);
-	}
-	for (index = low; index < high; index += high)
-	{
-		printf("Value checked array[%d] = [%d]\n", (int)index, array[low]);
-		if (value <= array[high] || !array[high])
-		{
-			printf("Value found between indexes [%d] and [%d]\n", (int)low, (int)high);
-			for (i = low; i <= high; i++)
-			{
-				if (i < size)
-					printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
-				if (array[i] == value)
-					return (i);
-			}
-		}
-		else
-			return (j_s_helper(array, high, high + sqrt(size - 1), value, size));
-	}
-	return (-1);
-}
-
-/**
  * jump_search - Uses the jump search algorithm to search
  * for a value in an array of integers.
  * @array: Array to search
@@ -51,7 +11,33 @@ int j_s_helper(int *array, size_t low, size_t high, int value, size_t size)
  */
 int jump_search(int *array, size_t size, int value)
 {
-	if (!array)
-		return (-1);
-	return (j_s_helper(array, 0, sqrt(size - 1), value, size));
+	size_t index = 0, low = 0, high, step = sqrt(size - 1);
+	int found = 1;
+
+	high = step;
+
+	while (index < size)
+	{
+		printf("Value checked array[%d] = [%d]\n", (int)index, array[index]);
+		if (index > high || size == 0)
+			return (-1);
+		if (value <= array[high] || high > size)
+		{
+			if (found)
+				printf("Value found between indexes [%d] and [%d]\n", (int)low, (int)high);
+			found = 0;
+			if (array[index] == value)
+				return (index);
+			if (index == size - 1)
+				high = 0;
+			else
+				index++;
+			continue;
+		}
+		low = high;
+		high += step;
+		index = low;
+		found = 1;
+	}
+	return (-1);
 }
